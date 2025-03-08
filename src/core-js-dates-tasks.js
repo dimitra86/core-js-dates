b2784d91f3b74b743996c1e23fbf50fc433577ee
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-03-08 22:59:47 +0300

    feat: finish isDateInPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -182,8 +182,17 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const startTimestamp2 = new Date(period.start);
+  const endTimestamp2 = new Date(period.end);
+  const curDateTimestamp = new Date(date);
+  if (
+    startTimestamp2 <= curDateTimestamp &&
+    curDateTimestamp <= endTimestamp2
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
