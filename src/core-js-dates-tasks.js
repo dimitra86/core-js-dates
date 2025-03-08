8079351c8bc748b6c8baeb647fecaea8fd1924bd
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-03-08 22:36:37 +0300

    feat: finish getCountDaysInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -127,8 +127,25 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  if (
+    month === 1 ||
+    month === 3 ||
+    month === 5 ||
+    month === 7 ||
+    month === 8 ||
+    month === 10 ||
+    month === 12
+  ) {
+    return 31;
+  }
+  if (month === 2) {
+    if (year % 4 === 0) {
+      return 29;
+    }
+    return 28;
+  }
+  return 30;
 }
 
 /**
